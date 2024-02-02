e7bd5ed45c1f76caa0969acecb34753605f1e40d
Jun Tan <133255971+thomastanjun@users.noreply.github.com>
2024-02-02 11:24:50 -0800

    Add file for demo

---
+++ sky_region.py
@@ -0,0 +1,45 @@
+import gradio as gr
+import cv2
+import numpy as np
+
+def sky(img):
+
+    # Convert the image to HSV color space
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+
+    # Print the region's mean hsv value    
+    left = (1400, 100)  
+    right = (1500, 200)  
+    
+    h,l, _ =img.shape
+    region_hsv = img_hsv[left[1]:right[1], left[0]:right[0]]
+
+    print("imageshape", h, l)    
+    print("Hue: ", np.mean(region_hsv[:, :, 0]))
+    print("Saturation: ", np.mean(region_hsv[:, :, 1]))
+    print("Value: ", np.mean(region_hsv[:, :, 2]))
+    
+    
+    # Threshold on the color range of sky
+    lower_thresh = np.array([90, 20, 180])
+    upper_thresh = np.array([190, 230, 255])
+    sky_region = cv2.inRange(img_hsv, lower_thresh, upper_thresh)
+
+    # Morphological closing and opening to erase noise in the background and the sky region 
+    kernel = np.ones((5, 5), np.uint8)
+    background_cleaned = cv2.morphologyEx(sky_region, cv2.MORPH_CLOSE, kernel)
+    sky_cleaned = cv2.morphologyEx(background_cleaned, cv2.MORPH_OPEN, kernel)
+
+
+    result = cv2.bitwise_and(img, img, mask=sky_cleaned)
+    
+    return result
+    
+demo = gr.Interface(
+    fn=sky,
+    inputs=["image"],
+    outputs=["image"],
+)
+
+demo.launch()
