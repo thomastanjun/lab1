73f87822c9220da79676b4c0d87709962a75b57a
Jun <thomastanjun@gmail.com>
2024-02-03 00:50:40 -0800

    updated thresholding range

--- sky_region.py
+++ sky_region.py
@@ -7,7 +7,7 @@ def sky(img):
     # Convert the image to HSV color space
     img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
-
+    '''
     # Print the region's mean hsv value    
     left = (1400, 100)  
     right = (1500, 200)  
@@ -19,10 +19,10 @@ def sky(img):
     print("Hue: ", np.mean(region_hsv[:, :, 0]))
     print("Saturation: ", np.mean(region_hsv[:, :, 1]))
     print("Value: ", np.mean(region_hsv[:, :, 2]))
-    
+    '''
     
     # Threshold on the color range of sky
-    lower_thresh = np.array([90, 20, 180])
+    lower_thresh = np.array([90, 20, 160])
     upper_thresh = np.array([190, 230, 255])
     sky_region = cv2.inRange(img_hsv, lower_thresh, upper_thresh)
 
@@ -31,7 +31,7 @@ def sky(img):
     background_cleaned = cv2.morphologyEx(sky_region, cv2.MORPH_CLOSE, kernel)
     sky_cleaned = cv2.morphologyEx(background_cleaned, cv2.MORPH_OPEN, kernel)
 
-
+    # Use the sky region to the original image
     result = cv2.bitwise_and(img, img, mask=sky_cleaned)
     
     return result
